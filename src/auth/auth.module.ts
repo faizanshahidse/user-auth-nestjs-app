6753c0e3472f2b0030fbd2112a5f0d8d6fe5a0da
divsync-faizans <faizans@divsync.com>
2022-03-12 16:54:45 +0500

    debugging configuration added, auth controller added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   providers: [AuthResolver, AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
