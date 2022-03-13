d9ab8d6cd41c2e95eb20b5352c666b3020677712
divsync-faizans <faizans@divsync.com>
2022-03-13 20:35:12 +0500

    jwt strategy and AuthGuard added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,18 +10,25 @@ import { UserModule } from '../user/user.module';
 import { UserService } from '../user/user.service';
 import { UserSchema } from '../schemas/user.schema';
 import { LocalStrategy } from '../strategies/local.strategy';
+import { JwtStrategy } from '../strategies/jwt.strategy';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: process.env.jwtSecret,
+      secret: process.env.JWT_Secret,
       signOptions: { expiresIn: '3600s' },
     }),
     UserModule,
   ],
-  providers: [AuthResolver, AuthService, UserService, LocalStrategy],
+  providers: [
+    AuthResolver,
+    AuthService,
+    UserService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ src/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from '../models/user.model';
+
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_Secret,
+    });
+  }
+
+  validate(email: string): Promise<User> {
+    return this.userService.getUserByEmail(email);
+  }
+}
